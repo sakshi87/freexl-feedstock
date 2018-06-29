fe516f1b623687e1efd47d8d39a25169d692a3bc
Filipe Fernandes <ocefpaf@gmail.com>
2018-06-29 10:48:20 -0300

    try libiconv only for Windows

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-export CFLAGS="-I$PREFIX/include -L$PREFIX/lib -O2 -Wl,-S $CFLAGS"
-export LDFLAGS="-L$PREFIX/lib -Wl,-rpath,${PREFIX}/lib $LDFLAGS"
-
 ./configure --prefix=$PREFIX --host=$HOST --build=$BUILD
 
 make -j$CPU_COUNT
