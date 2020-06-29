f72a76e9d609ec2a74d3838014c5baa3c9511189
odidev <odidev@puresoftware.com>
2020-06-29 14:27:35 +0000

    Add support for aarch64 and ppc64le

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+if [ $(uname -m) = 'aarch64' ]
+then
+./configure --prefix=$PREFIX --host=aarch64-linux-gnu --build=aarch64-linux-gnu
+else
 ./configure --prefix=$PREFIX --host=$HOST --build=$BUILD
+fi
 
 make -j$CPU_COUNT
 make check -j$CPU_COUNT
